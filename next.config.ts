a59992b5d0dccb3dba9d336e88e3f64060ad8295
ashreim <117382044+ashreim@users.noreply.github.com>
2026-04-06 18:31:13 +0400

    Infrastructure: Enabled Auto-Deployment via GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+  images: {
+    unoptimized: true, 
+  },
+  // If you are deploying to a subfolder like /yogacandy/, uncomment the next line:
+  // basePath: '/yogacandy',
 };
 
 export default nextConfig;
